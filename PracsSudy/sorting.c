afc4ffe1c4f9466f2f878c366ef258951e0cda57
no-good-username <avnishcabral@gmail.com>
2023-02-13 21:04:25 +0530

    added merge sorting

--- PracsSudy/sorting.c
+++ PracsSudy/sorting.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define MAX 10  
 
 
 int insort(int arr[]){
@@ -16,9 +16,55 @@ int insort(int arr[]){
     }
 }
 
-int mergesort(){
-    int low1;
+void merge_sort(int arr[], int low, int up){
+
+int mid;
+int temp[MAX];
+if(low<up){
+
+mid = (low+up)/2;
+merge_sort( arr, low , mid );
+merge_sort( arr, mid+1, up );
+merge( arr, temp, low, mid, mid+1, up );
+copy(arr,temp,low, up);
+
 }
+
+}
+
+void merge( int arr[], int temp[], int low1, int up1, int low2, int up2 ){
+
+int i = low1;
+int j = low2;
+int k = low1 ;
+while( (i <= up1) && (j <=up2) ){
+if(arr[i] <= arr[j])
+
+temp[k++] = arr[i++] ;
+
+else
+
+temp[k++] = arr[j++] ;
+
+}
+while( i <= up1 )
+
+temp[k++]=arr[i++];
+
+while( j <= up2 )
+
+temp[k++]=arr[j++];
+
+}
+void copy(int arr[], int temp[], int low, int up ){
+
+int i;
+for(i=low; i<=up; i++)
+arr[i]=temp[i];
+
+}   
+
+
 
 int main(){
     int arr[10] = {0,1,7,3,2,9,4,1,8,5};
